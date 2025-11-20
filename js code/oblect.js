4cf7911c09e55a08260dc51702d305ddeb8f9e72
Aitgul <ajtgulcharypkan@example.com>
2025-11-20 12:24:31 +0500

    object homework

---
+++ js code/oblect.js
@@ -0,0 +1,70 @@
+let person = {
+   name: "John",
+   age: 30,
+   city: "Tokyo"
+};
+console.log(person);
+//2
+let originalObject = { name: "John", age: 30, city: "Tokyo" };
+let copyObject = Object.assign({}, originalObject);
+
+copyObject.city = "Kannam";
+
+console.log(originalObject.city); 
+console.log(copyObject.city); 
+//3
+const personman = {
+    name: "Khardin",
+    age: 19,
+    city: "London",
+    
+    greet: function() {
+        console.log(`Привет, меня зовут ${this.name}, мне ${this.age} лет!`);
+    }
+};
+personman.greet();
+
+//4
+const obj1 = { a: 10, b: 20 };
+const obj2 = { a: 10, b: 20 };
+console.log(obj1 == obj2);  
+console.log(obj1 === obj2); 
+//В JavaScript объекты сравниваются по ссылке, а не по содержимому
+//5
+const book = {
+    title: "Such different world",
+    author: "Feia.Moren",
+    details: {
+        year: 2023,
+        pages: 271
+    }
+};
+const copyBook = Object.assign({}, book);
+copyBook.details.year = 2025;
+console.log(book.details.year);    
+console.log(copyBook.details.year); 
+//Object.assign() делает поверхностную копию объекта.Это значит, что сам объект копируется, но вложенные объекты остаются общими для обеих переменных
+//6
+const calculator = {
+    a: 5,
+    b: 3,
+    sum: function() {
+        return this.a + this.b;
+    },
+    multiply: function() {
+        return this.a * this.b;
+    }
+};
+
+console.log("Сумма:", calculator.sum());       
+console.log("Произведение:", calculator.multiply()); 
+//7
+const car = {
+    brand: "Toyota",
+    model: "Corolla"
+};
+car.model = "Camry";
+console.log(car); 
+//В JavaScript использование ключевого слова const не предотвращает изменение содержимого объектов. 
+// const только гарантирует, что переменная не может быть переназначена на другой объект , но свойства и элементы
+//  внутри него могут быть изменены.
